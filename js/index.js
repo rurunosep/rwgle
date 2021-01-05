b67337bb446abfe46a98dbbe24dda8140dec1a53
Kevin Hernandez-Rives <rurunosep@gmail.com>
2021-01-05 05:08:15 -0500

    Set up basic WebGL boilerplate

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 import('../wasm/').then((lib) => {
-  console.log(lib.add(2, 3))
+  const canvas = document.getElementById('canvas')
+
+  let engine
+  try {
+    engine = new lib.RustWebGLEngine(canvas)
+  } catch (e) {
+    console.log(e)
+  }
+
+  const render = () => {
+    engine.render()
+    window.requestAnimationFrame(render)
+  }
+
+  window.requestAnimationFrame(render)
 })
