c269736a9f0b395d0f483804c32943ded484bf9c
Kevin Hernandez-Rives <rurunosep@gmail.com>
2021-01-08 12:04:24 -0500

    Reduced program setup and render boilerplate

--- rust/src/simple_3d.rs
+++ rust/src/simple_3d.rs
@@ -1,23 +1,16 @@
-use super::utils::link_program;
+use super::utils::*;
 use nalgebra_glm as na;
+use std::collections::HashMap;
 use wasm_bindgen::JsCast;
 use web_sys::WebGlRenderingContext as GL;
 use web_sys::{HtmlCanvasElement, WebGlBuffer, WebGlProgram, WebGlUniformLocation};
 
-// TODO: holy shit, figure out a way to organize and automate all these fucking uniforms and shit
-
 pub struct Simple3D {
   program: WebGlProgram,
-  num_indices: i32,
-  position_buffer: WebGlBuffer,
-  normal_buffer: WebGlBuffer,
   index_buffer: WebGlBuffer,
-  world_location: WebGlUniformLocation,
-  view_location: WebGlUniformLocation,
-  projection_location: WebGlUniformLocation,
-  world_inverse_transpose_location: WebGlUniformLocation,
-  camera_world_position_location: WebGlUniformLocation,
-  reverse_light_direction_location: WebGlUniformLocation,
+  num_indices: i32,
+  attributes: HashMap<String, Attribute>,
+  uniform_locations: HashMap<String, WebGlUniformLocation>,
 }
 
 impl Simple3D {
@@ -28,74 +21,35 @@ impl Simple3D {
       include_str!("./shaders/simple_3d.frag"),
     )?;
 
-    let world_location = gl.get_uniform_location(&program, "u_world").unwrap();
-    let view_location = gl.get_uniform_location(&program, "u_view").unwrap();
-    let projection_location = gl.get_uniform_location(&program, "u_projection").unwrap();
-    let world_inverse_transpose_location = gl
-      .get_uniform_location(&program, "u_world_inverse_transpose")
-      .unwrap();
-    let camera_world_position_location = gl
-      .get_uniform_location(&program, "u_camera_world_position")
-      .unwrap();
-    let reverse_light_direction_location = gl
-      .get_uniform_location(&program, "u_reverse_light_direction")
-      .unwrap();
+    let attributes = get_attributes(&gl, &program);
+    let uniform_locations = get_uniform_locations(&gl, &program);
 
-    let positions = cube_positions();
-    let position_buffer = gl.create_buffer().unwrap();
-    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&position_buffer));
-    unsafe {
-      let array = js_sys::Float32Array::view(&positions);
-      gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &array, GL::STATIC_DRAW);
-    }
+    buffer_attribute_data(&gl, &attributes, "a_position", &cube_positions());
+    buffer_attribute_data(&gl, &attributes, "a_normal", &cube_normals());
 
-    let normals = cube_normals();
-    let normal_buffer = gl.create_buffer().unwrap();
-    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&normal_buffer));
-    unsafe {
-      let array = js_sys::Float32Array::view(&normals);
-      gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &array, GL::STATIC_DRAW);
-    }
-
-    let indices = cube_indices();
-    let index_buffer = gl.create_buffer().unwrap();
-    gl.bind_buffer(GL::ELEMENT_ARRAY_BUFFER, Some(&index_buffer));
-    unsafe {
-      let array = js_sys::Uint16Array::view(&indices);
-      gl.buffer_data_with_array_buffer_view(GL::ELEMENT_ARRAY_BUFFER, &array, GL::STATIC_DRAW);
-    }
-
-    let num_indices = indices.len() as i32;
+    let (index_buffer, num_indices) = buffer_index_data(&gl, &cube_indices());
 
     Ok(Simple3D {
       program,
-      num_indices,
-      position_buffer,
-      normal_buffer,
       index_buffer,
-      world_location,
-      view_location,
-      projection_location,
-      world_inverse_transpose_location,
-      camera_world_position_location,
-      reverse_light_direction_location,
+      num_indices,
+      attributes,
+      uniform_locations,
     })
   }
 
   pub fn render(&self, gl: &GL) {
-    let time = web_sys::window().unwrap().performance().unwrap().now() as f32;
     gl.use_program(Some(&self.program));
-
-    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&self.position_buffer));
-    gl.vertex_attrib_pointer_with_i32(0, 3, GL::FLOAT, false, 0, 0);
-    gl.enable_vertex_attrib_array(0);
-
-    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&self.normal_buffer));
-    gl.vertex_attrib_pointer_with_i32(1, 3, GL::FLOAT, false, 0, 0);
-    gl.enable_vertex_attrib_array(1);
-
+    set_attrib_pointers(&gl, &self.attributes);
     gl.bind_buffer(GL::ELEMENT_ARRAY_BUFFER, Some(&self.index_buffer));
+    self.load_uniforms(gl);
+    gl.draw_elements_with_i32(GL::TRIANGLES, self.num_indices, GL::UNSIGNED_SHORT, 0);
+  }
 
+  fn load_uniforms(&self, gl: &GL) {
+    let time = web_sys::window().unwrap().performance().unwrap().now() as f32;
+
+    // World Uniform
     let origin_shift = na::translation(&na::vec3(0., 0., 0.));
     let scale = na::scaling(&na::vec3(100., 100., 100.));
     let rotation = na::rotation(0., &na::vec3(1., 0., 0.))
@@ -106,14 +60,24 @@ impl Simple3D {
       * na::rotation(0.5, &na::vec3(0., 0., 1.));
     let translation = na::translation(&na::vec3(0., 0., -1000.));
     let world = translation * rotation * scale * origin_shift;
-    gl.uniform_matrix4fv_with_f32_array(Some(&self.world_location), false, &world.as_slice());
+    gl.uniform_matrix4fv_with_f32_array(
+      Some(&self.uniform_locations.get("u_world").unwrap()),
+      false,
+      &world.as_slice(),
+    );
 
+    // View Uniform
     let camera_rotation = na::rotation(0., &na::vec3(0., 1., 0.));
     let camera_position = na::vec3(0., 0., 0.);
     let camera_translation = na::translation(&camera_position);
     let view = na::inverse(&(camera_translation * camera_rotation));
-    gl.uniform_matrix4fv_with_f32_array(Some(&self.view_location), false, &view.as_slice());
+    gl.uniform_matrix4fv_with_f32_array(
+      Some(&self.uniform_locations.get("u_view").unwrap()),
+      false,
+      &view.as_slice(),
+    );
 
+    // Projection Uniform
     let canvas = gl
       .canvas()
       .unwrap()
@@ -123,30 +87,46 @@ impl Simple3D {
     let aspect = (canvas.width() / canvas.height()) as f32;
     let projection = na::perspective(aspect, fov, 1., 2000.);
     gl.uniform_matrix4fv_with_f32_array(
-      Some(&self.projection_location),
+      Some(&self.uniform_locations.get("u_projection").unwrap()),
       false,
       &projection.as_slice(),
     );
 
+    // World Inverse Transpose Uniform
     let world_inverse_transpose = na::transpose(&na::inverse(&world));
     gl.uniform_matrix4fv_with_f32_array(
-      Some(&self.world_inverse_transpose_location),
+      Some(
+        &self
+          .uniform_locations
+          .get("u_world_inverse_transpose")
+          .unwrap(),
+      ),
       false,
       &world_inverse_transpose.as_slice(),
     );
 
+    // Camera World Position Uniform
     gl.uniform3fv_with_f32_array(
-      Some(&self.camera_world_position_location),
+      Some(
+        &self
+          .uniform_locations
+          .get("u_camera_world_position")
+          .unwrap(),
+      ),
       camera_position.as_slice(),
     );
 
-    let reverse_light_direction = na::normalize(&na::vec3(0.5, 0.7, 1.0));
+    // Reverse Light Direction Uniform
+    let reverse_light_direction = na::normalize(&na::vec3(0.7, 0.5, 1.0));
     gl.uniform3fv_with_f32_array(
-      Some(&self.reverse_light_direction_location),
+      Some(
+        &self
+          .uniform_locations
+          .get("u_reverse_light_direction")
+          .unwrap(),
+      ),
       reverse_light_direction.as_slice(),
     );
-
-    gl.draw_elements_with_i32(GL::TRIANGLES, self.num_indices, GL::UNSIGNED_SHORT, 0);
   }
 }
 
--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,7 +1,13 @@
+use std::collections::HashMap;
 use web_sys::WebGlRenderingContext as GL;
-use web_sys::{WebGlProgram, WebGlShader};
+use web_sys::{WebGlBuffer, WebGlProgram, WebGlShader, WebGlUniformLocation};
 
-// TODO: only check compile status if linking fails?
+pub struct Attribute {
+  pub index: u32,
+  pub size: i32,
+  pub type_: u32,
+  pub buffer: WebGlBuffer,
+}
 
 pub fn link_program(
   gl: &GL,
@@ -41,5 +47,79 @@ pub fn compile_shader(gl: &GL, shader_type: u32, source: &str) -> Result<WebGlSh
     Ok(shader)
   } else {
     Err(gl.get_shader_info_log(&shader).unwrap())
+  }
+}
+
+pub fn get_attributes(gl: &GL, program: &WebGlProgram) -> HashMap<String, Attribute> {
+  let num_attributes = gl
+    .get_program_parameter(&program, GL::ACTIVE_ATTRIBUTES)
+    .as_f64()
+    .unwrap() as u32;
+  let mut map = HashMap::new();
+  for index in 0..num_attributes {
+    let info = gl.get_active_attrib(&program, index).unwrap();
+    let (size, type_) = match info.type_() {
+      GL::FLOAT_VEC3 => (3, GL::FLOAT),
+      x => panic!("No match for attribute type: {}", x),
+    };
+    map.insert(
+      info.name(),
+      Attribute {
+        index,
+        size,
+        type_,
+        buffer: gl.create_buffer().unwrap(),
+      },
+    );
+  }
+  map
+}
+
+pub fn get_uniform_locations(
+  gl: &GL,
+  program: &WebGlProgram,
+) -> HashMap<String, WebGlUniformLocation> {
+  let num_uniforms = gl
+    .get_program_parameter(&program, GL::ACTIVE_UNIFORMS)
+    .as_f64()
+    .unwrap() as u32;
+  let mut map = HashMap::new();
+  for i in 0..num_uniforms {
+    let name = gl.get_active_uniform(&program, i).unwrap().name();
+    let location = gl.get_uniform_location(&program, &name).unwrap();
+    map.insert(name, location);
+  }
+  map
+}
+
+pub fn buffer_attribute_data(
+  gl: &GL,
+  attributes: &HashMap<String, Attribute>,
+  name: &str,
+  data: &[f32],
+) {
+  let buffer = &attributes.get(name).unwrap().buffer;
+  gl.bind_buffer(GL::ARRAY_BUFFER, Some(&buffer));
+  unsafe {
+    let array = js_sys::Float32Array::view(&data);
+    gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &array, GL::STATIC_DRAW);
+  }
+}
+
+pub fn buffer_index_data(gl: &GL, data: &[u16]) -> (WebGlBuffer, i32) {
+  let buffer = gl.create_buffer().unwrap();
+  gl.bind_buffer(GL::ELEMENT_ARRAY_BUFFER, Some(&buffer));
+  unsafe {
+    let array = js_sys::Uint16Array::view(&data);
+    gl.buffer_data_with_array_buffer_view(GL::ELEMENT_ARRAY_BUFFER, &array, GL::STATIC_DRAW);
+  }
+  (buffer, data.len() as i32)
+}
+
+pub fn set_attrib_pointers(gl: &GL, attributes: &HashMap<String, Attribute>) {
+  for a in attributes.values() {
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&a.buffer));
+    gl.vertex_attrib_pointer_with_i32(a.index, a.size, a.type_, false, 0, 0);
+    gl.enable_vertex_attrib_array(a.index);
   }
 }
